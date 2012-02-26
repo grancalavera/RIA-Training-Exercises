2d840515805aeb9591412ff741a90e991fc5a068
grancalavera <leoncoto@gmail.com>
2012-02-26 14:32:53 +0000

    General abstraction / modularization of the code. Basic objectives reached.

--- ria/static/ria/js/facebook.js
+++ ria/static/ria/js/facebook.js
@@ -1,21 +1,29 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: RIA.fbAppId,
-        status: true, 
-        cookie: true,
-        xfbml: true,
-        oauth: true,
-    });
+(function(){
+    facebook = _.namespace('ria.facebook');
+    facebook.init = function(appId, callback){
+        var js, id = 'facebook-jssdk';
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId: appId,
+                status: true, 
+                cookie: true,
+                xfbml: true,
+                oauth: true,
+            });
 
-    // This can be passed as part of the template, in the future
-    // RIA.facebookCallback();
-    $('#content').append(_.template($('#fb-login-template').html()));
-    console.log('append fb-login-template');
-};
-
-(function(d){
-    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    d.getElementsByTagName('head')[0].appendChild(js);
-}(document));
+            FB.getLoginStatus(function(response) {
+                if (callback) {
+                    callback(response);
+                }
+            });
+        }
+        if (document.getElementById(id)) {
+            return;
+        }
+        js = document.createElement('script'); 
+        js.id = id; 
+        js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        document.getElementsByTagName('head')[0].appendChild(js);
+    }
+})();
