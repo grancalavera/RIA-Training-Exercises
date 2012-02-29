bf4391be3594ebe7c3cd5e60d7c71dda98b13f1a
Leon Coto <leoncoto@gmail.com>
2012-02-29 16:40:06 +0000

    Rendering page from template. Dependencies loading from STATIC_URL

--- ria/views.py
+++ ria/views.py
@@ -2,8 +2,10 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context # Allow serving files from /static/
+from django.shortcuts import render
 
 def home(request):
-	t = get_template('home.html')
-	html = t.render(Context({}))
+	# t = get_template('home.html')
+	# html = t.render(request, Context({}))
+	html = render(request, 'home.html', Context({}))
 	return HttpResponse(html)
