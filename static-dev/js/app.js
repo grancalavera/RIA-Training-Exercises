128ad9b3b852fef1bee266ae06ac7b555e6836c2
Leon Coto <leoncoto@gmail.com>
2012-03-01 12:55:08 +0000

    Adding event support to Facebook module.

--- static-dev/js/app.js
+++ static-dev/js/app.js
@@ -2,17 +2,32 @@ define([
     'jQuery', 'Underscore', 'Backbone',
     'facebook/facebook',
     'text!../templates/hello-world.html', 
-    'text!../templates/fb-root.html',
     'text!../templates/simple-text.html'], 
     function(
         $, _, Backbone, facebook, 
-        t_hello, t_fb_root, t_text){
+        t_hello, t_text){
 
         var initialize = function (){
             $(document).ready(function(){
-                $('body').html(_.template(t_hello)({message: 'Masmelo!'}));
-                $('body').append(_.template(t_fb_root));
+                var loginButton;
+
+                $('body').html(_.template(t_hello)({message: 'Razorfish!'}));
+                loginButton = $('body').append(facebook.loginButton());
+                $('.fb-login-button').hide();
+
+                facebook.events.on('facebook:init', function(message){
+                    console.log('from app > facebook:init ( ' + message + ' )');
+                });
+
                 facebook.init('216629731768132', function(response){
+                    switch(response.status) {
+                        case 'connected':
+                            break;
+                        case 'not-authorized':
+                        case 'unknown':
+                        default:
+                            $('.fb-login-button').show();
+                    }
                     $('body').append(_.template(t_text)({text: response.status}));
                 });
             });
--- static-dev/js/facebook/facebook.js
+++ static-dev/js/facebook/facebook.js
@@ -1,8 +1,48 @@
-define([], function(){
-	var init;
-	
-	init = function(appId, callback){
-        var js, id = 'facebook-jssdk';
+define(
+[
+    // Libraries
+    'jQuery', 'Backbone', 'Underscore',
+
+    // Templates
+    'text!facebook/fb-root.html',
+    'text!facebook/fb-login.html'
+], 
+
+function(
+    $, Backbone, _, 
+    t_fbRoot, t_login){
+	var isInit, init, t, loginButton, events, on, off;
+
+    // Make it an event dispatcher
+    events = {};
+    _.extend(events, Backbone.Events);
+
+    /**
+     * @private
+     * Templates
+     */
+    t = {
+        root: _.template(t_fbRoot),
+        login: _.template(t_login)
+    }
+    
+    /**
+     * Initializes the facebook module
+     * @param appId {String} The facebook app id
+     * @param callback {Function} [Optional] A function to be executed once the facebook module has been initialized.
+     */
+    init = function(appId, callback){
+        var script, id;
+        if (isInit) {
+            return;
+        }
+
+        id = 'facebook-jssdk';
+
+        if (!$('#fb-root').length) {
+            $('body').append(t.root());
+        }
+
         window.fbAsyncInit = function() {
             FB.init({
                 appId: appId,
@@ -17,18 +57,35 @@ define([], function(){
                     callback(response);
                 }
             });
+
+            events.trigger('facebook:init', 1);
+            events.trigger('facebook:init', 2);
+            events.trigger('facebook:init', 3);
+            events.trigger('facebook:init', 4);
+            isInit = true;
         }
-        if (document.getElementById(id)) {
+
+        if ($(id).length) {
             return;
         }
-        js = document.createElement('script'); 
-        js.id = id; 
-        js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        document.getElementsByTagName('head')[0].appendChild(js);
+
+        script = document.createElement('script'); 
+        script.id = id; 
+        script.async = true;
+        script.src = "//connect.facebook.net/en_US/all.js";
+        $('head').append(script);
 	};
 
+    /**
+     * Returns a compiled template for the login button
+     */
+    loginButton = function () {
+        return t.login();
+    }
+
 	return {
-		'init': init
+		'init': init, 
+        'loginButton': loginButton,
+        'events': events
 	};
 })
